5a9076911aa5aaa198e4b0ea30e38dc312afafd7
Arijit0 <bagarijit0@gmail.com>
2025-08-28 18:40:26 +0530

    "feat: configure vite for multi-page deployment"

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 export default defineConfig({
   plugins: [tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        buttlift: resolve(__dirname, "buttlift-page.html"),
+      },
+    },
+  },
 });
